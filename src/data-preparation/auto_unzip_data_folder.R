0774b6cca78f47b51cb4a773c1a6feaa7a5f1982
Gijs Moonen <gmoonen1997@gmail.com>
2022-03-16 20:42:54 +0100

    add automated unzip script and deleted some test files

---
+++ src/data-preparation/auto_unzip_data_folder.R
@@ -0,0 +1,16 @@
+## Unzip data files
+
+install.packages("here")
+install.packages("plyr")
+
+library(here)
+library(plyr)
+library(conflicted)
+
+conflict_prefer("here", "here")
+
+zip_file <- list.files(path = here("data/airbnb"), pattern = "*.zip",
+                                    full.names = T)
+
+ldply(.data = zip_file, .fun = unzip, exdir = here("data/airbnb"))  
+
